4d7677b402afd8ef1e2af45b2a8beff9fed84b40
Lars Palinckx <lpalinckx@gmail.com>
2026-02-22 20:29:59 +0100

    fix: remove debugging db

--- src/core/subtitle_indexer.py
+++ src/core/subtitle_indexer.py
@@ -21,7 +21,7 @@ class SubtitleDatabase(pykka.ThreadingActor):
         self.conn: duckdb.DuckDBPyConnection
 
     def on_start(self) -> None:
-        self.conn = duckdb.connect(self.db_path) if self.db_path != "" else duckdb.connect("my_db.duckdb")
+        self.conn = duckdb.connect(self.db_path) if self.db_path != "" else duckdb.connect()
 
         cursor = self.conn.cursor()
 
